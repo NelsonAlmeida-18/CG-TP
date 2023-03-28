3625d07e8de4b5121bb8c4c538443b3d9255f85b
Gonçalo da Silva <63649917+Goncalo448@users.noreply.github.com>
2023-03-28 18:59:04 +0100

    Delete drawing.h

--- phase2/engine/Drawing/drawing.h
+++
@@ -1,4 +0,0 @@
-void tokenize(std::string const &str, const char* delim, std::vector<float> &out);
-int drawFigure(std::string filename, GLuint *buffers, int buffer);
-std::vector<int> drawModels(Scene scene, GLuint *buffers);
-void drawAxis();
